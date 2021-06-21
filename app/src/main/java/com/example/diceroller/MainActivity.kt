8cab792c3bc9938aa33558bca47a8a868385a95f
huiqing990905 <qing05099@gmail.com>
2021-06-21 20:21:34 +0800

    Coding Challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,7 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        var countUpButton: Button = findViewById(R.id.count_up_button)
         rollButton.setOnClickListener { rollDice() }
+        countUpButton.setOnClickListener { countUp()}
+
     }
 
     private fun rollDice(){
@@ -20,5 +23,22 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
         Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if(resultText.text == "Hello World!")
+        {
+            resultText.text = "1";
+        }else
+        {
+            var resultInt = resultText.text.toString().toInt()
+            if(resultInt < 6)
+            {
+                resultInt++;
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
